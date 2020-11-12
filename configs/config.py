c042ac7b015a7c59862a9fb12488ce858dc88361
Daniel Gao <245784521@qq.com>
2020-11-12 21:25:25 +0800

    move mmdet

--- configs/config.py
+++ configs/config.py
@@ -1,16 +1,19 @@
 from easydict import EasyDict
 
 shelf_model = EasyDict(dict(
-    device = "gpu",
-    model_path= "",
+    device = "cuda",
+    model_config="",
+    model_checkpoint= "",
 ))
 
 similarity_model =  EasyDict(dict(
-    device = "gpu",
-    model_path= "",
+    device = "cuda",
+    model_config="",
+    model_checkpoint= "",
 ))
 
 offtake_model = EasyDict(dict(
-    device="gpu",
-    model_path="",
+    device = "cuda",
+    model_config="",
+    model_checkpoint= "",
 ))
